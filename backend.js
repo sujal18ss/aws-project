6ed4e922e370c5399c3df49c44d8d899044fd46a
sujal18ss <95114364+sujal18ss@users.noreply.github.com>
2023-08-23 00:38:38 +0530

    Update backend.js

--- backend.js
+++ backend.js
@@ -4,8 +4,8 @@ function create(){
          cname = document.getElementById("container_name").value
          img = document.getElementById("image_name").value
          xhr = new XMLHttpRequest();
-         xhr.open('GET','http://15.206.116.52/cgi-bin/create.py?i=' +img+ '&n=' +cname,true);
-        //  http://15.207.254.199/cgi-bin/create.py?i
+         xhr.open('GET','http://13.233.244.12/cgi-bin/create.py?i=' +img+ '&n=' +cname,true);
+        
          xhr.send();
          xhr.onload=function (){
              output = xhr.responseText;
@@ -20,12 +20,12 @@ function del(){
     if(decision == true){
             cname = document.getElementById("del_name").value
             xhr = new XMLHttpRequest();
-            xhr.open('GET','http://15.206.116.52/cgi-bin/del.py?n= '+cname,true);
+            xhr.open('GET','http://13.233.244.12/cgi-bin/del.py?n= '+cname,true);
             // http://15.207.254.199/cgi-bin/del.py?n
             xhr.send();
             xhr.onload=function (){
                 output = xhr.responseText;
-		    output = "Container" + output + "Deleted";
+                    output = "Container " + output + " Deleted";
                 document.getElementById("output_id").innerHTML = output;
             }
     }
@@ -35,12 +35,12 @@ function run(){
 
 i=document.getElementById("command_name").value
 var xhr=new XMLHttpRequest();
-xhr.open('GET', 'http://15.206.116.52/cgi-bin/run.py?x='+i,true);
-// http://15.207.254.199/cgi-bin/run.py?x
+xhr.open('GET', 'http://13.233.244.12/cgi-bin/run.py?x='+i,true);
+
 xhr.send();
 xhr.onload=function(){
         var output=xhr.responseText;
-	
-        document.getElementById("output_id").innerHTML=output;
+
+        document.getElementById("output_id1").innerHTML=output;
 }
 }
